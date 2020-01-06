6a161a5c693e8ace8f671b254b26f2320391067f
Aniketh01 <anikethgireesh@gmail.com>
2020-01-06 18:06:43 +0530

    Automated compile script

---
+++ compile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+echo "Run configure..."
+PKG_CONFIG_PATH=/home/devbox/devel/quic_research/build_openssl/lib/pkgconfig:/home/devbox/devel/quic_research/nghttp3_build/lib/pkgconfig:/home/devbox/devel/quic_research/ngtcp2/lib ./configure
+echo "make..."
+make -j$(nproc)
+echo "make install..."
+make install
+echo
+echo "Compiled successfully!!!"
+echo
