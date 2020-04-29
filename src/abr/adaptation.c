c3af4e6bea118ef43f2a945d449967e04fa416bc
Aniketh01 <anikethgireesh@gmail.com>
2020-04-29 15:23:00 +0530

    ABR: Skelton code for Adaptation algorithm with DASH

---
+++ src/abr/adaptation.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * calculate_rate_index that finds the bitrate closes to the curr_rate
+ * param bitrates: LIst of available bitrates
+ * param curr_rate: current_bitrate
+ * return: curr_rate_index
+ */
+
+int calculate_rate_index(int bitrates, int curr_rate)
+{
+
+}
+
+/**
+ * basic_dash to predict the next_bitrate using the basic_dash algorithm
+ * param segment_number: Current segment number
+ * param bitrates: A tuple/list of available bitrates
+ * param average_dwn_time: Average download time observed so far
+ * param segment_download_time:  Time taken to download the current segment
+ * param curr_rate: Current bitrate being used
+ * return: next_rate : Bitrate for the next segment
+ * return: updated_dwn_time: Updated average downlaod time
+*/
+
+int basic_dash(int segment_number, int bitrates, int average_dwn_time, int segment_download_time, int curr_rate)
+{
+
+}
+
+
+
