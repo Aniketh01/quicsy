4c7c8158f37f7e6df9bd8865ea5530bd33bc1822
Aniketh01 <anikethgireesh@gmail.com>
2020-05-06 14:47:52 +0530

    Script/ABR: Add few user cli arguments

--- scripts/abr/manifest.py
+++ scripts/abr/manifest.py
@@ -11,6 +11,9 @@ start_time = time.time()
 logging.basicConfig(filename='manifest_creation-' +
                     str(start_time) + '.log', level=logging.DEBUG)
 
+source = 'Big_Buck_Bunny_1080_10s_10MB.mp4'
+prefix = '/dash/'
+framerate = 60
 
 def check_and_create(dir_path):
 	print ("Checking: %s" % dir_path)
@@ -23,10 +26,26 @@ def check_and_create(dir_path):
 
 def main():
     parser = argparse.ArgumentParser()
-
+	parser.add_argument('--prefix', '-p', help='Prefix')
+	parser.add_argument('--fps', help="Frames per second to use for re-encoding")
     parser.add_argument('-i', '--input', required=True,
                         help='The path to the video file (required).')
     args = parser.parse_args()
+
+    if args.prefix:
+        if not args.prefix.endswith('/'):
+            prefix = args.prefix + '/'
+        else:
+            prefix = args.prefix
+    else:
+        print ("No prefix given")
+
+    if args.input:
+		source = args.input 
+
+    if args.fps:
+		framerate = args.fps
+
 
     logging.debug("input: " + args.input + ", datetime: " + str(datetime.datetime.now()))
 
