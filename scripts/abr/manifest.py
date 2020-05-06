812c7af430104cb02161543341eb2d696b3fc2bb
Aniketh01 <anikethgireesh@gmail.com>
2020-05-06 14:43:15 +0530

    Script/ABR: Initial: check and create dir

--- scripts/abr/manifest.py
+++ scripts/abr/manifest.py
@@ -3,11 +3,22 @@ import sys
 import logging
 import datetime
 import time
+import os
+from threading import Thread
 
 start_time = time.time()
 
 logging.basicConfig(filename='manifest_creation-' +
                     str(start_time) + '.log', level=logging.DEBUG)
+
+
+def check_and_create(dir_path):
+	print ("Checking: %s" % dir_path)
+	if not os.path.isdir(dir_path):
+		print ('Destination directory: %s does not exist, creating one' % dir_path)
+		os.mkdir(dir_path)
+	else:
+		print ('Found directory: %s ' % dir_path)
 
 
 def main():
@@ -17,8 +28,7 @@ def main():
                         help='The path to the video file (required).')
     args = parser.parse_args()
 
-    logging.debug("input: " + args.input
-                  + ", datetime: " + str(datetime.datetime.now()))
+    logging.debug("input: " + args.input + ", datetime: " + str(datetime.datetime.now()))
 
 
 if __name__ == "__main__":
